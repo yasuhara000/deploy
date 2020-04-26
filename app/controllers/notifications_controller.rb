02031f2eeb32eddc7de29f35452ff82b42b26ec9
yasuhara000 <kaaazumakun123@gmail.com>
2020-04-26 03:06:39 +0000

    バリデーションの設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,19 +1,45 @@
 class NotificationsController < ApplicationController
-    before_action :authenticate_user!, only: [:index]
-    before_action :authenticate_engineer!, only: [:index]
+    
+    before_action :set_current_user
+
+    def set_current_user
+       
+        if user_signed_in?
+            @current_user = User.find_by(id: params[:user_id])
+            if @current_user != current_user
+                redirect_to questions_path
+            end
+        elsif engineer_signed_in?
+            @current_engineer = Engineer.find_by(id: params[:engineer_id])
+            if @current_engineer != current_engineer
+                redirect_to questions_path
+            end
+        else
+            redirect_to questions_path
+        end
+    end
+    
     def index
         #current_userの質問に紐づいた通知一覧
         if user_signed_in? 
             @notifications = current_user.passive_notifications.page(params[:page]).per(10)
             @notifications.where(checked: false).each do |notification|
                 notification.update_attributes(checked: true)
+                if notification.visited_id != current_user
+                    redirect_to questions_path
+                end
             end
+            
          #current_engineerの回答に紐づいた通知一覧
         elsif engineer_signed_in? 
             @notifications = current_engineer.passive_notifications.page(params[:page]).per(10)
             @notifications.where(checked: false).each do |notification|
                 notification.update_attributes(checked: true)
+                if notification.e_visited_id!= current_engineer
+                    redirect_to questions_path
+                end
             end
+        
         end
     end
 
